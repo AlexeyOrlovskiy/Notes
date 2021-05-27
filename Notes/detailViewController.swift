11469dec5f7d37d86bad229522b818d455a0329f
Alexey Orlovskiy <alexeypronin94@yandex.ru>
2021-05-27 17:25:10 +0300

    Change TextView options

--- Notes/detailViewController.swift
+++ Notes/detailViewController.swift
@@ -19,11 +19,31 @@ class detailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         actionButton.setTitle(buttonTitle, for: .normal)
-        detailedNote.textContainer.lineFragmentPadding = 10
+        detailedNote.layer.cornerRadius = 15
+        detailedNote.textContainer.lineFragmentPadding = 15
         detailedNote.text = passedNoteText ?? "Введите текст заметки..."
         
         noteToCreate = (buttonTitle == "Сохранить") ? true : false
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        self.view.endEditing(true)
+    }
+}
+
+extension detailViewController: UITextViewDelegate {
+    
+    func detailNoteBeginEditing(_ detailNote: UITextView) {
+        detailNote.backgroundColor = .white
+        detailNote.textColor = .gray
+    }
+    
+    func detailNoteDidEndEditing(_ detailNote: UITextView) {
+        detailNote.backgroundColor = .systemGray5
+        detailNote.textColor = .black
     }
 }
